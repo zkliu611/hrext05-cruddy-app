b9467b63503f14bc58302335c7458118919653ce
ZK Liu <zkliu@ZKs-Mac.local>
2018-10-30 15:33:03 -0700

    working on add entry

--- app.js
+++ app.js
@@ -7,24 +7,44 @@ update display with new text value
  */
 
 $(document).ready(function(){
-  console.log("before\n", window.localStorage);
 
-  // add event listener
-  $(".add-text-btn").on("click", function(){
-    $(".show-text").empty();
-    var curTextValue = $('#theKey').val(); // reading from <input>
-    var curKeyValue = "theKey"; // change to dynamic key?
-    localStorage.setItem(curKeyValue, curTextValue);
-    $(".show-text").append(curTextValue);
+  function readFormData() {
+    var formData = {};
+    formData["itemName"] = $("#item-name").val();
+    formData["sku"] = $("#sku").val();
+    formData["department"] = $("#department").val();
+    formData["quantity"] = $("#item-quantity").val();
+    formData["dateReceived"] = $("#date-received").val();
+    return formData;
+  }
+
+  $(".add-item").on("click", function(){
+    var formData = readFormData();
+    console.log(formData);
+    
   });
 
-  // remove item from app
 
-  // listen for click event (del)
-  $(".clear-cache-btn").on("click", function(){
-    // clear local storage
-    localStorage.clear();
-    $(".show-text").empty();
-  });
+
+
+  // console.log("before\n", window.localStorage);
+
+  // // add event listener
+  // $(".add-text-btn").on("click", function(){
+  //   $(".show-text").empty();
+  //   var curTextValue = $('#theKey').val(); // reading from <input>
+  //   var curKeyValue = "theKey"; // change to dynamic key?
+  //   localStorage.setItem(curKeyValue, curTextValue);
+  //   $(".show-text").append(curTextValue);
+  // });
+
+  // // remove item from app
+
+  // // listen for click event (del)
+  // $(".clear-cache-btn").on("click", function(){
+  //   // clear local storage
+  //   localStorage.clear();
+  //   $(".show-text").empty();
+  // });
 
 });
